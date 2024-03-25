6d80b82c472aba22b3f498a3c62057a0d98eb744
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2024-03-25 08:12:23 -0300

    fibonacci

--- main.cpp
+++ main.cpp
@@ -5,7 +5,30 @@ using std::cout;
 using std::endl;
 using std::string;
 
+int Fibonacci(int iIteracoes);
+
 int main() 
 {
+  cout << Fibonacci(5);
+
   return 0;
+}
+
+int Fibonacci(int iIteracoes){
+  // se for apenas um passo retornamos 0...
+  if (iIteracoes == 1){
+    return 0;
+  }
+  
+  // senão fazemos as iterações parando um passo antes (para compensar
+  // o primeiro if) para o resto da sequência
+  int iAnterior = 0;
+  int iResultado = 1;
+
+  for (int i = iIteracoes; i > 1; i--){
+    iResultado += iAnterior;  
+    iAnterior = iResultado - iAnterior;
+  }
+
+  return iResultado;
 }
