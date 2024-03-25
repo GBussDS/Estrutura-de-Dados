7438a17dda68ca357e31a3d9a956f0cb3c51e4ec
Vinícius Nascimento <124941430+Vinasque@users.noreply.github.com>
2024-03-25 08:01:53 -0300

    Função "abs" criada

--- main.cpp
+++ main.cpp
@@ -5,7 +5,23 @@ using std::cout;
 using std::endl;
 using std::string;
 
+int abs(int iNum);
+
 int main() 
 {
-  return 0;
+    int iValor = -25;
+    int iValorAbsoluto = abs(iValor);
+    cout << "Valor absoluto de " << iValor << ": " << iValorAbsoluto << endl;
+    
+    return 0;
+}
+
+int abs(int iNum)
+{
+    if (iNum < 0)
+    {
+        iNum = (-1) * iNum;
+    }
+    
+    return iNum;
 }
